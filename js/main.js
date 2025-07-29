0a1d5c6214f9a4d8c05726c8948cca5154eac6fc
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-07-29 10:40:19 -0500

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header-content");
+  const scrollY = window.scrollY;
+
+  if (scrollY < window.innerHeight) {
+    const opacity = 1 - scrollY / window.innerHeight;
+    header.style.opacity = opacity;
+  } else {
+    header.style.opacity = 0;
+  }
+});
